63b0065a25acf2101ef42d50bd1517f945b0ee18
Daaagama <66567121+Daaagama@users.noreply.github.com>
2021-12-07 10:51:45 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GVBilokorenko/edu-dis-labs.git master:gh-pages
+git push -f git@github.com:Daaagama/ris-labs.git master:gh-pages
 
 cd -
